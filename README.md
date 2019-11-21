7addd74d1a819f1f5ed2a49055587ddbff934fad
bitweft <bitweft@gmail.com>
2019-11-21 20:44:08 +0800

    Add test to open a web page using selenium webdriver

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## Traditional vs Visual AI Tests
 
 This repo is to see how testing a basic login and home page using Visual AI differs from the traditional approach 
+
+#### Pre-requisites
+- Install the correct version of [chrome driver](https://github.com/SeleniumHQ/selenium/wiki/ChromeDriver)
--- src/test/java/TraditionalTests.java
+++ src/test/java/TraditionalTests.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TraditionalTests {
+
+    @Test
+    public void openLoginPage() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demo.applitools.com/hackathon.html");
+    }
 }
